d2c5e24b30dc2b9a385c1e4ed8ceb25cf823df8c
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-03-09 19:44:26 +0000

    Hacky proof of concept

--- src/scriptBlock/scrollToCurrentCloze.js
+++ src/scriptBlock/scrollToCurrentCloze.js
@@ -16,3 +16,61 @@ var onShownHook
     setTimeout(scrollToCloze, 10)
   }
 })()
+
+// Check if "foo" is in localStorage
+if(localStorage.getItem("foo")) {
+    // If "foo" exists, increment its value by 1
+    localStorage.setItem("foo", parseInt(localStorage.getItem("foo")) + 1);
+} else {
+    // If "foo" doesn't exist, set its value to 0
+    localStorage.setItem("foo", 0);
+}
+console.log( localStorage.getItem("foo") );
+
+
+// Generate a random number between 1 and 100
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// Apply class based on whether the random number is even or odd
+if (randomNumber % 2 === 0) {
+  //console.log(document.getElementById('.CLOZE2_W'))
+
+} else {
+ // console.log(document.getElementById('.CLOZE2_W'))
+}
+
+var allElements = document.querySelectorAll('*');
+
+//console.log(document.querySelectorAll('cloze'));
+
+//console.log(document.querySelectorAll('cloze_w'));
+
+// Loop through each element and log its tag name
+document.querySelectorAll('cloze_w').forEach(function(element) {
+//    console.log(element.tagName);
+});
+
+
+
+
+// Get all elements in the document
+var allElements = document.querySelectorAll('*');
+
+// Convert the NodeList to an array for easier manipulation
+var elementsArray = Array.from(allElements);
+
+// Log each element to the console
+//elementsArray.forEach(function(element) {
+//  element.style.color = "green";
+//    console.log(element);
+//});
+//document.body.style.backgroundColor = "red";
+//var all = document.querySelectorAll('cloze2_w');
+//
+//for (var i=0, max=all.length; i < max; i++) {
+// all[i].style.backgroundColor = "yellow";
+//}
+
+document.querySelectorAll('cloze2_w').forEach(element => {
+  element.style.backgroundColor = "yellow";
+});
